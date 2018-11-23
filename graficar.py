40ad6a7f4554dfc6be3e10a930da0ea7f9d3a818
Andres <a.martinez@uniandes.edu.co>
2018-11-23 07:29:12 -0500

    is

--- graficar.py
+++ graficar.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 A = np.genfromtxt("a.txt")
 B = np.genfromtxt("b.txt")
 
-plt.hist(A,bins=100,label="Serial")
-plt.hist(B,bins=100,alpha=0.5,label="Paralelo")
+plt.hist(A,bins=100,label="Serial",normed=True)
+plt.hist(B,bins=100,alpha=0.5,label="Paralelo",normed=True)
 plt.legend()
 plt.savefig("ComparacionA.pdf")
